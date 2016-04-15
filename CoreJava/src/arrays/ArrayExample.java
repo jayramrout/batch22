273fd0b2438143bf041727338b54e186c59095a5
Jayram <jayram.rout@hotmail.com>
2016-04-14 21:37:59 -0400

    Update

---
+++ CoreJava/src/arrays/ArrayExample.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		int jayAge = 10;
+		int tanuAge = 20;
+		
+//		int size = Integer.parseInt(args[0]);
+		
+		String jayName = "Jayram";
+		String tanuName = "Tanu";
+		
+		int ages[] = new int[5];
+		
+		System.out.println(ages.length);
+		
+		
+		
+		
+	}
+
+}
