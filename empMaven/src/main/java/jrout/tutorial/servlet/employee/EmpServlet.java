7d7dddcca109e4293a7a4b761ae44dab62aef745
Jayram <jayram.rout@hotmail.com>
2016-06-09 21:56:26 -0400

    Update

---
+++ empMaven/src/main/java/jrout/tutorial/servlet/employee/EmpServlet.java
@@ -0,0 +1,157 @@
+package jrout.tutorial.servlet.employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jrout.tutorial.servlet.dao.EmployeeDAOImpl;
+import jrout.tutorial.servlet.dao.IEmployeeDAO;
+import jrout.tutorial.servlet.domain.Employee;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/EmpServlet")
+public class EmpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EmpServlet() {
+		super();
+		System.out.println("I am been created as a first servlet");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("I am been initialized");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+
+		String fname = req.getParameter("fname");
+		String lname = req.getParameter("lname");
+		String password = req.getParameter("pwd");
+		System.out.println(fname + " " + lname + " " + password);
+
+		int deptId = Integer.parseInt(req.getParameter("deptId"));
+		IEmployeeDAO employeeDAO = new EmployeeDAOImpl();
+		List<Employee> employeeList = employeeDAO.getEmpWithDepartMent(deptId);
+
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println(
+				"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<table class=\"table table-striped table-bordered\">");
+		// out.println("<table style=\"width:100%\">");
+		out.println("<thead>");
+		out.println("<tr>");
+		out.println("<th>EMPLOYEE_ID</th>");
+		out.println("<th>FIRST Name</th>");
+		out.println("<th>Last Name</th>");
+		out.println("<th>Email</th>");
+		out.println("</tr>");
+		out.println("</thead>");
+		out.println("<tbody>");
+
+		for (Employee emp : employeeList) {
+			out.println("<tr><td class=\"info\">" + emp.getEmployeeId() + "</td><td>" + emp.getFname() + "</td><td>"
+					+ emp.getLname() + "</td><td>" + emp.getEmail() + "</td></tr>");
+		}
+		out.println("</tbody>");
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	/*
+	 * @Override protected void service(HttpServletRequest request,
+	 * HttpServletResponse response) throws ServletException, IOException {
+	 * PrintWriter out = response.getWriter();
+	 * response.setContentType("text/html");
+	 * //http://localhost:8082/HelloEmployee/EmpServlet?deptId=60 String fname =
+	 * request.getParameter("fname"); String lname =
+	 * request.getParameter("lname"); String password =
+	 * request.getParameter("pwd"); System.out.println(fname +" " + lname + " "+
+	 * password );
+	 * 
+	 * int deptId = Integer.parseInt(request.getParameter("deptId")); Connection
+	 * con = null; ResultSet rs = null; Statement stmt = null;
+	 * 
+	 * try { Class.forName("oracle.jdbc.driver.OracleDriver"); } catch
+	 * (ClassNotFoundException var18) { var18.printStackTrace(); }
+	 * 
+	 * try { con =
+	 * DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "HR",
+	 * "HR"); stmt = con.createStatement(); rs = stmt.executeQuery(
+	 * "select EMPLOYEE_ID , FIRST_NAME , LAST_NAME , EMAIL from employees where DEPARTMENT_ID = "
+	 * + deptId); out.println("<!DOCTYPE html>"); out.println("<html>");
+	 * out.println("<head>"); out.println(
+	 * "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">"
+	 * ); out.println("</head>"); out.println("<body>"); out.println(
+	 * "<table class=\"table table-striped table-bordered\">"); //out.println(
+	 * "<table style=\"width:100%\">"); out.println("<thead>");
+	 * out.println("<tr>"); out.println("<th>EMPLOYEE_ID</th>"); out.println(
+	 * "<th>FIRST Name</th>"); out.println("<th>Last Name</th>");
+	 * out.println("<th>Email</th>"); out.println("</tr>");
+	 * out.println("</thead>"); out.println("<tbody>");
+	 * 
+	 * while(rs.next()) { out.println("<tr><td class=\"info\">" + rs.getInt(1) +
+	 * "</td><td>" + rs.getString(2) + "</td><td>" + rs.getString(3) +
+	 * "</td><td>" + rs.getString(4) + "</td></tr>"); }
+	 * 
+	 * out.println("</tbody>"); out.println("</table>"); out.println("</body>");
+	 * out.println("</html>"); } catch (SQLException exception) {
+	 * exception.printStackTrace(); } finally { if(con != null) { try {
+	 * con.close(); } catch (SQLException exception) {
+	 * exception.printStackTrace(); } }
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
+
+	@Override
+	public void destroy() {
+		System.out.println("I am been destroyed");
+	}
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	/*
+	 * protected void doGet(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException {
+	 * //response.getWriter().append("Served at: "
+	 * ).append(request.getContextPath()); response.getWriter().println(
+	 * "Hello EMployee"); }
+	 * 
+	 *//**
+		 * @see HttpServlet#doPost(HttpServletRequest request,
+		 *      HttpServletResponse response)
+		 *//*
+		 * protected void doPost(HttpServletRequest request, HttpServletResponse
+		 * response) throws ServletException, IOException { // TODO
+		 * Auto-generated method stub doGet(request, response); }
+		 */
+
+}
---
+++ empMaven/src/main/webapp/employeeSalaryManager.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.List" %>
+<%@ page import="jrout.tutorial.servlet.domain.Employee" %>	
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	crossorigin="anonymous">
+</head>
+<body>
+	<%
+		List<Employee> employeeList = (List<Employee>)request.getAttribute("employeeList");
+	%>
+	
+	<table class="table table-striped table-bordered">
+		<thead>
+			<tr>
+				
+				<th>FIRST Name</th>
+				<th>Salary</th>
+				<th>Manager</th>
+			</tr>
+		</thead>
+		<tbody>
+<!-- 		out.println("<tr><td>" + emp.getFname() + "</td><td>"
+						+ emp.getSalary() + "</td><td>" + emp.getManagerId() + "</td></tr>");
+ -->
+ 		<%
+			for(Employee emp : employeeList){
+		%>
+			<tr>
+				<td class="info" bgcolor="black"><%=emp.getFname()%></td>
+				<td><%=emp.getSalary()%></td>
+				<td><%=emp.getManagerId()%></td>
+			</tr>
+		<%		
+			}
+		%>
+			<!-- <tr>
+				<td class="info">100</td>
+				<td>Steven</td>
+				<td>King</td>
+				<td>SKING</td>
+			</tr> -->
+		</tbody>
+	</table>
+</body>
+</html>
